c6a8924a62ebc011c8c22cb66540697613abd144
hippocampa <tsdhrm@outlook.com>
2023-06-27 15:20:09 +0800

    first commmit

---
+++ lua/options.lua
@@ -0,0 +1,17 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = false
+vim.opt.cursorline = true
+vim.opt.ruler = true
+vim.opt.scrolloff = 8
+vim.opt.autoindent = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.showmode = true
